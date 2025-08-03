64ea8874710f09fe34befd5bc14aeb09d61ca8fc
namratha-26 <namratha.ip@wissen.com>
2025-08-03 19:54:57 +0530

    cat gifs for every commit

--- extension.js
+++ extension.js
@@ -1,4 +1,6 @@
 const vscode = require('vscode');
+const https = require('https');
+const fallbackUrl = 'https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif';
 
 function activate(context) {
     // Use the console to output diagnostic information (console.log) and errors (console.error)
@@ -13,12 +15,77 @@ function activate(context) {
         return;
     }
 
-    git.repositories.forEach(repo => {
-        repo.onDidCommit(() => {
-            console.log(`Commit detected in repo: ${repo.rootUri.fsPath}`);
-            vscode.window.showInformationMessage('🟢 A Git commit was made!');
-        });
+    if (git.repositories && git.repositories.length > 0) {
+		git.repositories.forEach(repo => {
+			console.log('Setting commit listener for repo:', repo.rootUri.fsPath);
+			repo.onDidCommit(() => {
+			console.log('Commit detected event fired!');
+			vscode.window.showInformationMessage('🟢 A Git commit was made!');
+			showCommitCelebration()
+		});
+	});
+	} else {
+		console.log('No git repositories found at activation time.');
+	}
+
+	git.onDidOpenRepository(repo => {
+		console.log('New repo opened:', repo.rootUri.fsPath);
+		repo.onDidCommit(() => {
+		console.log('Commit detected event fired!');
+		vscode.window.showInformationMessage('🟢 A Git commit was made!');
+		showCommitCelebration()
+		});
+  	});
+
+}
+
+function showCommitCelebration() {
+  const panel = vscode.window.createWebviewPanel(
+    'gitCommitCelebration',
+    '🎉 Commit Celebration!',
+    vscode.ViewColumn.One,
+    { enableScripts: true }
+  );
+
+  fetchRandomCatUrl(url => {
+	console.log('URl: ',url)
+    panel.webview.html = getWebviewContent(url);
+
+    setTimeout(() => {
+      panel.dispose();
+    }, 10000);
+  });
+}
+function fetchRandomCatUrl(callback) {
+  https.get('https://api.thecatapi.com/v1/images/search?mime_types=gif', res => {
+    let data = '';
+
+    res.on('data', chunk => data += chunk);
+    res.on('end', () => {
+      try {
+        const [cat] = JSON.parse(data);
+        callback(cat?.url || fallbackUrl);
+      } catch {
+        callback(fallbackUrl);
+      }
     });
+
+  }).on('error', () => callback(fallbackUrl));
+}
+
+function getWebviewContent(catImageUrl) {
+	return `<!DOCTYPE html>
+		<html lang="en">
+		<head>
+		<meta charset="UTF-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<title>Cat Celebration</title>
+		</head>
+		<body style="display:flex; justify-content:flex-start; align-items:flex-start; height:100vh; margin:0; padding:20px;">
+		<img src="${catImageUrl}" width="500" alt="Random Cat Meme" />
+		</body>
+		</html>`;
+
 }
 
 function deactivate() {}
