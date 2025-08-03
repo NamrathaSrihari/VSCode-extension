6909d2f2d21357dced0621d04a055d7718b8a91b
namratha-26 <namratha.ip@wissen.com>
2025-08-03 14:35:46 +0530

    adding custom extension

---
+++ extension.js
@@ -0,0 +1,42 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+const vscode = require('vscode');
+
+// This method is called when your extension is activated
+// Your extension is activated the very first time the command is executed
+
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+
+function activate(context) {
+
+	// Use the console to output diagnostic information (console.log) and errors (console.error)
+	// This line of code will only be executed once when your extension is activated
+	console.log('Congratulations, your extension "gitCommitFun" is now active!');
+	const gitExtension = vscode.extensions.getExtension('vscode.git')?.exports;
+	const git = gitExtension?.getAPI(1);
+
+	if (!git) {
+		vscode.window.showErrorMessage('Git Extension not available');
+		return;
+	}
+
+	// Listen for Git operations
+	git.onDidRunGitOperation((e) => {
+		// GitOperation.Commit = 2 (not exported directly in JS)
+		if (e.operation === 2) {
+			console.log('🟢 Git commit detected!');
+			vscode.window.showInformationMessage('🟢 A Git commit was made!');
+		}
+	});
+
+}
+
+// This method is called when your extension is deactivated
+function deactivate() {}
+
+module.exports = {
+	activate,
+	deactivate
+}
