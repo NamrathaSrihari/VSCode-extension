56a9c40addef8b35aef0123a3ebafcab42cf13f8
namratha-26 <namratha.ip@wissen.com>
2025-08-03 15:02:21 +0530

    using git api ver 2

--- extension.js
+++ extension.js
@@ -15,7 +15,7 @@ function activate(context) {
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "gitCommitFun" is now active!');
 	const gitExtension = vscode.extensions.getExtension('vscode.git')?.exports;
-	const git = gitExtension?.getAPI(1);
+	const git = gitExtension?.getAPI(2);
 
 	if (!git) {
 		vscode.window.showErrorMessage('Git Extension not available');
